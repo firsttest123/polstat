9a74ddb0ea8aeb049d515f465896807a4799a8b1
firsttest123 <48312248+firsttest123@users.noreply.github.com>
2019-03-19 13:19:28 +0530

    added lora

---
+++ ver1_4.c
@@ -0,0 +1,22 @@
+
+
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+
+#include <C8051F020.h>                 // SFR declarations
+#include <stdio.H>
+#include <string.H>
+#include <math.H>
+#include <INTRINS.H>
+void main()
+{
+	int project2;
+	int y;
+	//its is version 1_5
+
+yes 
+no
+up down
+}
