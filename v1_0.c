71afbe6ed94d365aa9b7a0a4e83438d84d00c62e
firsttest123 <48312248+firsttest123@users.noreply.github.com>
2019-03-19 18:08:19 +0530

    Add files via upload

---
+++ v1_0.c
@@ -0,0 +1,16 @@
+
+
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+
+#include <C8051F020.h>                 // SFR declarations
+#include <stdio.H>
+#include <string.H>
+#include <math.H>
+#include <INTRINS.H>
+void main()
+{
+	int project1;
+}
